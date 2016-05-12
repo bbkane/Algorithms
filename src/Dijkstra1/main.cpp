3f0809fb342a3bad3f0906d103d9157480760ec7
Ben Kane's Win Tower <bbk1524@gmail.com>
2016-05-12 01:53:27 -0500

    Think I fixed Dijkstra. Almost ready for next

--- src/Dijkstra1/main.cpp
+++ src/Dijkstra1/main.cpp
@@ -1,7 +1,17 @@
+//This code seems to work, but is bad for two reasons:
+//- it uses iterators to connections. Iterators get invalidated when their containers are modified.
+//  This means I can't modify my containers after I start the traversal
+//- it stores everything in one big vector, and uses an enum class (List) to determine list.
+//  This means that all searches through the list search the whole thing
+//Improvements for next time:
+//- use a vector of Connections and and lists of DijkstraConnections (with pointers to the underlying connections)
+//  so I can avoid that linear search. I still won't be able to modify the underlying list, but I can't think of a way around it.
+// Error handling
+// - final node not reachable
+
 #include <fmt-3.0.0/fmt/format.h>
 #include <vector>
 #include <limits>
-#include <array>
 #include <iostream>
 
 enum class List
@@ -46,10 +56,11 @@ int main()
 
     if (start_id == end_id)
     {
-        fmt::print("Already there!\n");
+        std::cout << "Already at end!" << std::endl;
         return 0;
     }
 
+    // Add all starting connections to Frontier and get the cheapest one
     auto cheapest = connections.end();
     int lowest_cost = std::numeric_limits<int>::max();
     for (auto it = connections.begin(); it != connections.end(); ++it)
@@ -66,12 +77,14 @@ int main()
         }
     }
 
+    //for printing later
     auto first = cheapest;
 
     while (true)
     {
         cheapest->list = List::Closed;
 
+        // If we're done, print
         if (cheapest->second == end_id)
         {
             cheapest->next_connection = connections.end();
@@ -81,25 +94,29 @@ int main()
             }
             return 0;
         }
-
-        auto new_cheapest = connections.end();
-        lowest_cost = std::numeric_limits<int>::max();
+        // Add all of cheapest's kids to frontier
         for (auto it = connections.begin(); it != connections.end(); ++it)
         {
             if (it->first == cheapest->second && it->list == List::Unexplored)
             {
                 it->total_cost = cheapest->total_cost + it->cost_between;
                 it->list = List::Frontier;
+                //set the child relationship here for printing later
                 cheapest->next_connection = it;
-                if (it->total_cost < lowest_cost)
-                {
-                    new_cheapest = it;
-                    lowest_cost = it->total_cost;
-                }
             }
         }
 
-        cheapest = new_cheapest;
+        // get the next cheapest
+        lowest_cost = std::numeric_limits<int>::max();
+        for (auto it = connections.begin(); it != connections.end(); ++it)
+        {
+            if (it->total_cost < lowest_cost && it->list == List::Frontier)
+            {
+                cheapest = it;
+                lowest_cost = it->total_cost;
+            }
+        }
+        
     }
 
 }
