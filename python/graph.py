08b9bce789b94c2901dd4b082a70c619f961e0dc
Benjamin Kane <bbk1524@gmail.com>
2017-05-05 22:19:45 -0500

    Format Python code. It looks like it's working :)

--- python/graph.py
+++ python/graph.py
@@ -7,6 +7,7 @@ Created on Mon Jul 11 21:32:46 2016
 
 from collections import namedtuple
 
+
 def dijkstra(start, end, get_neighbors):
     """
     Args:
@@ -15,26 +16,26 @@ def dijkstra(start, end, get_neighbors):
         node (type): must implement __hash__, __eq__, __str__
         get_neighbors(node) -> iterable of classes which have a cost: number member
             and a finish: node member
-            
+
     Returns:
         [node]: list of the nodes from start to finish (shortest path)
-    
+
     Raises:
         NoPathException: No path found
     """
-    
+
     class Element:
         # Can't use a namedtuple because it needs to be mutable
         def __init__(self, item, cost):
             self.item = item
             self.cost = cost
-        
+
         def __repr__(self):
             return 'Element(item=%r, cost=%r)' % (self.item, self.cost)
-            
+
     class NoPathException(Exception):
         pass
-    
+
     explored = set()
     frontier = []
     frontier.append(Element(start, 0))
@@ -43,12 +44,11 @@ def dijkstra(start, end, get_neighbors):
         if not frontier:
             raise NoPathException(str(start))
         # treat the list like a priority queue by sorting it by cost
-        frontier.sort(key = lambda e: e.cost)
+        frontier.sort(key=lambda e: e.cost)
         # pop the first element
         current_node = frontier.pop(0)
         print(current_node.item, current_node.cost)
-        if current_node.item == end: # We're done. Get the path and return
-#            return parents; # TODO: rm   
+        if current_node.item == end:  # We're done. Get the path and return
             path = []
             current = end
             while parents[current]:
@@ -69,19 +69,20 @@ def dijkstra(start, end, get_neighbors):
                             element.cost = total_neighbor_cost
                             parents[element.item] = current_node.item
                         break
-                else: # no break (the item wasn't in the frontier)
+                else:  # no break (the item wasn't in the frontier)
                     frontier.append(Element(neighbor.finish, total_neighbor_cost))
                     parents[neighbor.finish] = current_node.item
 
 
 Edge = namedtuple('Edge', ['start', 'finish', 'cost'])
 
+
 class Graph:
-    
+
     def __init__(self):
         self.nodes = set()
         self.edges = set()
-        
+
     def add_two_way(self, a, b, cost):
         self.nodes.add(a)
         self.nodes.add(b)
@@ -89,13 +90,12 @@ class Graph:
         self.edges.add(Edge(a, b, cost))
 
     def __str__(self):
-        node_str=  'nodes:\n' + '\n'.join(str(node) for node in self.nodes)
+        node_str = 'nodes:\n' + '\n'.join(str(node) for node in self.nodes)
         edge_str = 'edges:\n' + '\n'.join(str(edge) for edge in self.edges)
         return node_str + '\n' + edge_str
 
     def get_neighbors(self, current_node):
         yield from {e for e in self.edges if e.start == current_node}
-
 
 
 FIELD = """\
@@ -115,18 +115,20 @@ ooooooxooooooo
 xxxxxoxoxooooo
 osooooxooooeoo
 """
+
+
 class Field:
-    
+
     def __init__(self, field):
         self.field = [f.strip() for f in field.strip().split('\n')]
-    
+
     def get_location(self, char):
         for row_num, row in enumerate(self.field):
             for col_num, col in enumerate(row):
                 if col == char:
                     return (row_num, col_num)
         raise ValueError("%r not in Field" % char)
-        
+
     def get_neighbors(self, node):
         row, column = node
         neighbors = []
@@ -143,49 +145,55 @@ class Field:
                         # make sure nothing is in the way
                         if value != 'x':
                             # get the cost and add to neighbors
-                        
+
                             # the diagonal positions are the rows and columns
                             # shifted by two and the 't' positions are shifted
                             # by one. Summing everything and dividing by two
                             # is an easy check for both cases
                             if (abs(r + row + c + column) % 2 == 0):
-                                cost = 1.4 # diagonal
+                                cost = 1.4  # diagonal
                             else:
-                                cost = 1 # 't'
+                                cost = 1  # 't'
                             neighbors.append(Edge(None, (r, c), cost))
         return neighbors
-    
+
     def modify(self, *nodes):
         """turn (row, column) to 'p'"""
         for node in nodes:
             row, column = node
             self.field[row] = self.field[row][:column] + 'p' + self.field[row][column + 1:]
-    
+
     def __str__(self):
         # replace all but outer 'o's with spaces
         return '\n'.join(self.field)
-    
 
-g = Graph()
 
-g.add_two_way('1', '2', 7)
-g.add_two_way('1', '3', 9)
-g.add_two_way('1', '6', 14)
-g.add_two_way('2', '3', 10)
-g.add_two_way('2', '4', 15)
-g.add_two_way('3', '4', 11)
-g.add_two_way('3', '6', 2)
-g.add_two_way('4', '5', 6)
-g.add_two_way('5', '6', 9)
-    
-print(dijkstra('1', '5', g.get_neighbors))
+def test_graph():
+    g = Graph()
 
-#print(list(g.get_neighbors('2')))
-#f = Field(FIELD2)
-#print(f)
-#start = f.get_location('s')
-#end = f.get_location('e')
-#path = dijkstra(start, end, f.get_neighbors)
-#print(path)
-#f.modify(*path[1:-1])
-#print(f)
+    g.add_two_way('1', '2', 7)
+    g.add_two_way('1', '3', 9)
+    g.add_two_way('1', '6', 14)
+    g.add_two_way('2', '3', 10)
+    g.add_two_way('2', '4', 15)
+    g.add_two_way('3', '4', 11)
+    g.add_two_way('3', '6', 2)
+    g.add_two_way('4', '5', 6)
+    g.add_two_way('5', '6', 9)
+
+    print(dijkstra('1', '5', g.get_neighbors))
+
+
+def test_field():
+    f = Field(FIELD2)
+    print(f)
+    start = f.get_location('s')
+    end = f.get_location('e')
+    path = dijkstra(start, end, f.get_neighbors)
+    print(path)
+    f.modify(*path)
+    print(f)
+
+
+if __name__ == "__main__":
+    test_field()
